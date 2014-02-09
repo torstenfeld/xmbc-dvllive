37f363a723dd949276c64a37f85e09b3a228f29b
Torsten Feld <torsten@torsten-feld.de>
2014-02-09 21:34:00 +0100

    added basic stuff for plugin

---
+++ default.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import urllib
+import urllib2
+import re
+# import xbmcplugin
+# import xbmcaddon
+# import xbmcgui
+
+
+# addon = xbmcaddon.Addon('plugin.video.peoplesvoice')
+__author__ = 'Torsten'
+# __language__  = addon.getLocalizedString
+
+# def CATEGORIES():
+#     addDir(__language__(30010),'http://www.thepeoplesvoice.tv/watch-now/',1,'http://www.thepeoplesvoice.tv/sites/all/themes/tpv/images/tpv-logo-footer.gif',__language__(30013))
+#     addDir(__language__(30016),'http://www.thepeoplesvoice.tv/whatson/',3,'http://www.thepeoplesvoice.tv/sites/all/themes/tpv/images/tpv-logo-footer.gif',__language__(30013))
+#     addDir(__language__(30017),'http://www.thepeoplesvoice.tv/show/',4,'http://www.thepeoplesvoice.tv/sites/all/themes/tpv/images/tpv-logo-footer.gif',__language__(30013))
+#     addLink(__language__(30010)+__language__(30018),'http://cdn.rbm.tv:1935/rightbrainmedia-live-106/_definst_/ddstream_1/playlist.m3u8','http://www.thepeoplesvoice.tv/sites/all/themes/tpv/images/tpv-logo-footer.gif',__language__(30013)+__language__(30012))
+
+
---
+++ dvllive/web.py
@@ -0,0 +1,59 @@
+__author__ = 'Torsten'
+
+
+from bs4 import BeautifulSoup
+import urllib2
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+class Dvllive(object):
+    _page_videos = 'http://www.dvllive.tv/videos'
+
+    def __init__(self):
+        pass
+
+    def get_videos(self):
+        videos = []
+        page_html = urllib2.urlopen(self._page_videos)
+        page_soup = BeautifulSoup(page_html.read())
+        videos += self._get_video_per_page(page_soup)
+        pages = self._get_last_video_page(page_soup)
+        for page_number in pages:
+            sub_page_html = urllib2.urlopen(self._page_videos + '?page=' + str(page_number))
+            sub_page_soup = BeautifulSoup(sub_page_html.read())
+            videos += self._get_video_per_page(sub_page_soup)
+        pp.pprint(videos)
+
+    @staticmethod
+    def _get_last_video_page(soup):
+        last_page_number = int(soup.find('div', class_='pagination').span.find_all('a')[-2].span.text)
+        return range(2, last_page_number + 1)
+
+    @staticmethod
+    def _get_video_per_page(soup):
+        vids_on_this_page = []
+        tag_videos_items = soup.find('div', id='videos').ol
+        for list_item in tag_videos_items.contents:
+            try:
+                link = list_item.a
+            except AttributeError:
+                continue
+            else:
+                single_vid = {
+                    'link': link['href'],
+                    'title': link['title'].encode('utf-8'),
+                    'date': link.find('span', class_='date').string.encode('utf-8')
+                }
+                vids_on_this_page.append(single_vid)
+        return vids_on_this_page
+
+def main():
+    dvllive = Dvllive()
+    dvllive.get_videos()
+
+
+if __name__ == '__main__':
+    main()
+
