98a27fa612ef14d904d5217c9e50c68188c76e43
Torsten Feld <torsten@torsten-feld.de>
2014-03-02 15:00:12 +0100

    working on gui

--- addon.py
+++ addon.py
@@ -4,10 +4,10 @@
 # import urllib2
 # import re
 # import sys
-# import xbmc
-# import xbmcplugin
-# import xbmcaddon
-# import xbmcgui
+import xbmc
+import xbmcplugin
+import xbmcaddon
+import xbmcgui
 from dvllive.web import Dvllive
 from pyxbmct.addonwindow import *
 
@@ -15,21 +15,24 @@ from pyxbmct.addonwindow import *
 addon = xbmcaddon.Addon('plugin.video.dvllive')
 __author__ = 'Torsten'
 
-#get actioncodes from https://github.com/xbmc/xbmc/blob/master/xbmc/guilib/Key.h
-# ACTION_PREVIOUS_MENU = 10
-# ACTION_SELECT_ITEM = 7
-# ACTION_PARENT_DIR = 9
 
 
-class MyDisplay(AddonDialogWindow):
+class MyDisplay(AddonFullWindow):
+    hello_button = None
+    close_button = None
+    name_field = None
+    title_list = None
+    videos = []
+
     def __init__(self, title=''):
-        self._dvllive = Dvllive()
-        self._dvllive.get_videos()
-        self.videos = self._dvllive.videos_found
         # Call the base class' constructor.
         super(MyDisplay, self).__init__(title)
-        # Set width, height and the grid parameters
-        self.setGeometry(300, 230, 4, 2)
+
+        self._dvllive = Dvllive()
+        self.videos_refresh()
+
+        # Set width, height, row, columns
+        self.setGeometry(900, 630, 10, 5)
         # Call set controls method
         self.set_controls()
         # Call set navigation method.
@@ -37,39 +40,55 @@ class MyDisplay(AddonDialogWindow):
         # Connect Backspace button to close our addon.
         self.connect(ACTION_NAV_BACK, self.close)
 
+    def videos_refresh(self):
+        self._dvllive.get_videos()
+        self.videos = self._dvllive.videos_found
+
     def set_controls(self):
          """Set up UI controls"""
          # Image control
          image = Image('xbmc-logo.png')
-         self.placeControl(image, 0, 0, rowspan=2, columnspan=2)
+         self.placeControl(image, 0, 0, rowspan=2, columnspan=5)
+         # List
+         self.title_list = List()
+         self.placeControl(self.title_list, 2, 0, rowspan=7, columnspan=5)
+         # for i in range(1, 4):
+         for video in self.videos:
+            self.title_list.addItem(video['title'])
          # Text label
-         label = Label('Your name:')
-         self.placeControl(label, 2, 0)
+         # label = Label('Your name:')
+         # self.placeControl(label, 3, 0)
          # Text edit control
-         self.name_field = Edit('')
-         self.placeControl(self.name_field, 2, 1)
+         # self.name_field = Edit('')
+         # self.placeControl(self.name_field, 3, 1)
          # Close button
          self.close_button = Button('Close')
-         self.placeControl(self.close_button, 3, 0)
+         self.placeControl(self.close_button, 9, 0)
          # Connect close button
          self.connect(self.close_button, self.close)
          # Hello button.
-         self.hello_buton = Button('Hello')
-         self.placeControl(self.hello_buton, 3, 1)
+         self.hello_button = Button('Hello')
+         self.placeControl(self.hello_button, 9, 1)
          # Connect Hello button.
-         self.connect(self.hello_buton, lambda:
-         xbmc.executebuiltin('Notification(Hello %s!, Welcome to PyXBMCt.)' % self.videos[0]['title']))
+         # self.connect(self.hello_button, lambda: xbmc.executebuiltin('Notification(Hello %s!, Welcome to PyXBMCt.)' % self.videos[1]['title']))
          # xbmc.executebuiltin('Notification(Hello %s!, Welcome to PyXBMCt.)' % self.name_field.getText()))
 
     def set_navigation(self):
          """Set up keyboard/remote navigation between controls."""
-         self.name_field.controlUp(self.hello_buton)
-         self.name_field.controlDown(self.hello_buton)
-         self.close_button.controlLeft(self.hello_buton)
-         self.close_button.controlRight(self.hello_buton)
-         self.hello_buton.setNavigation(self.name_field, self.name_field, self.close_button, self.close_button)
+         self.title_list.controlLeft(self.hello_button)
+         # self.name_field.controlUp(self.hello_button)
+         # self.name_field.controlDown(self.hello_button)
+         self.close_button.controlLeft(self.hello_button)
+         self.close_button.controlRight(self.hello_button)
+         self.close_button.controlUp(self.title_list)
+         self.close_button.controlDown(self.title_list)
+         self.hello_button.controlLeft(self.close_button)
+         self.hello_button.controlRight(self.close_button)
+         self.hello_button.controlUp(self.title_list)
+         self.hello_button.controlDown(self.title_list)
+         # self.hello_button.setNavigation(self.name_field, self.name_field, self.close_button, self.close_button)
          # Set initial focus.
-         self.setFocus(self.name_field)
+         self.setFocus(self.title_list)
 
 
 mydisplay = MyDisplay()
