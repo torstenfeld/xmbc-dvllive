a07e6dfa7af6de61241c01c33b9f7099558407da
Torsten Feld <torsten@torsten-feld.de>
2014-03-16 20:04:29 +0100

    added mixpanel

--- addon.py
+++ addon.py
@@ -1,25 +1,98 @@
 # -*- coding: utf-8 -*-
 
-# import urllib
-# import urllib2
-# import re
-# import sys
-# import xbmc
-# import xbmcplugin
-# import xbmcaddon
-# import xbmcgui
-from xbmcswift2 import Plugin, xbmcaddon
+import re
+import random
+import string
+from xbmcswift2 import Plugin, xbmc, xbmcaddon
 from dvllive.web import Dvllive
-# from pyxbmct.addonwindow import *
+from mymixpanel import Mixpanel
+
+is_mixpanel_available = 'no'
+
+class MyMixpanel(object):
+    _mptoken = 'fed95492dc111b7805684b04ec2d0ce6'
+    # _mptoken = 'a0d1edc9e04bfea65ccce4ac9d99c62f'
+
+    def __init__(self):
+        self._mp = Mixpanel(self._mptoken)
+        global is_mixpanel_available
+        is_mixpanel_available = 'yes'
+        self._id = self._get_uniqueid()
+        self._ip = xbmc.getIPAddress()
+        self._lang = xbmc.getLanguage()
+        self._mp.people_set(self._id, {
+            'ip': self._ip,
+            'lang': self._lang
+        })
+        self.track('start')
+        return None
+
+    def _get_uniqueid(self):
+        if 'isRandom' in cache:
+            if not cache['isRandom']:
+                # self._id already correct
+                uid = cache['uid']
+            else:
+                # self._id not yet correct
+                if self._check_mac():
+                    cache['isRandom'] = False
+                    uid = cache['uid']
+                else:
+                    cache['isRandom'] = True
+                    cache['uid'] = self._create_randomid()
+                    uid = cache['uid']
+        else:
+            # neither correct nor random id
+            if self._check_mac():
+                cache['isRandom'] = False
+                uid = cache['uid']
+            else:
+                cache['isRandom'] = True
+                cache['uid'] = self._create_randomid()
+                uid = cache['uid']
+
+        for i in range(0, 10):
+            print 'mac: %s / uid: %s' % (cache['mac'], uid,)
+        return uid
+
+    @staticmethod
+    def _check_mac():
+        mac = xbmc.getInfoLabel('Network.MacAddress')
+        cache['mac'] = mac
+        if re.match('([a-hA-H0-9]{2}:){5}[a-hA-H0-9]{2}', mac):
+            cache['uid'] = mac
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def _create_randomid():
+        return ''.join(random.choice(string.letters) for i in range(40))
 
 
-addon = xbmcaddon.Addon('plugin.video.dvllive')
+    def track(self, event_name, properties={}, meta={}):
+        self._mp.track(self._id, event_name, properties, meta)
+        return self
+
+    def track_route(self, route_name):
+        self.track('route', properties={
+            'route': route_name
+        })
+
+addon_id = 'plugin.video.dvllive'
+addon = xbmcaddon.Addon(addon_id)
 
 plugin = Plugin()
+cache = plugin.get_storage('cache')
+mp = MyMixpanel()
 dvllive = Dvllive()
 
 @plugin.route('/')
 def index():
+    print 'is_mixpanel_available: ', is_mixpanel_available
+    mp.track('route', properties={
+        'route': 'index'
+    })
     items = []
     dvllive.get_videos()
     for video in dvllive.videos_found:
@@ -33,6 +106,10 @@ def index():
 
 @plugin.route('/showvideo/<stub>/')
 def show_video(stub):
+    mp.track('route', properties={
+        'route': 'show_video',
+        'stub': stub
+    })
     # name = urllib.unquote(safename)
     asset = dvllive.play_video(stub)
     # plugin.log.info('Playing url: %s' % 'asdf')
@@ -47,85 +124,15 @@ def show_video(stub):
 
 @plugin.route('/showrtmp/<url>/')
 def show_rtmp(url):
+    mp.track('route', properties={
+        'route': 'show_rtmp',
+        'stub': url
+    })
     item = {
         'label': 'rtmp',
         'path': 'rtmp://fms.12E5.edgecastcdn.net/0012E5/mp4:videos/8Juv1MVa-485.mp4'
     }
     return plugin.play_video(item)
-
-
-# class MyDisplay(AddonFullWindow):
-#     hello_button = None
-#     close_button = None
-#     name_field = None
-#     title_list = None
-#     videos = []
-#
-#     def __init__(self, title=''):
-#         # Call the base class' constructor.
-#         super(MyDisplay, self).__init__(title)
-#
-#         self._dvllive = Dvllive()
-#         self.videos_refresh()
-#
-#         # Set width, height, row, columns
-#         self.setGeometry(900, 630, 10, 5)
-#         # Call set controls method
-#         self.set_controls()
-#         # Call set navigation method.
-#         self.set_navigation()
-#         # Connect Backspace button to close our addon.
-#         self.connect(ACTION_NAV_BACK, self.close)
-#
-#     def videos_refresh(self):
-#         self._dvllive.get_videos()
-#         self.videos = self._dvllive.videos_found
-#
-#     def set_controls(self):
-#          """Set up UI controls"""
-#          # Image control
-#          image = Image('xbmc-logo.png')
-#          self.placeControl(image, 0, 0, rowspan=2, columnspan=5)
-#          # List
-#          self.title_list = List()
-#          self.placeControl(self.title_list, 2, 0, rowspan=7, columnspan=5)
-#          # for i in range(1, 4):
-#          for video in self.videos:
-#             self.title_list.addItem(video['title'])
-#          # Text label
-#          # label = Label('Your name:')
-#          # self.placeControl(label, 3, 0)
-#          # Text edit control
-#          # self.name_field = Edit('')
-#          # self.placeControl(self.name_field, 3, 1)
-#          # Close button
-#          self.close_button = Button('Close')
-#          self.placeControl(self.close_button, 9, 0)
-#          # Connect close button
-#          self.connect(self.close_button, self.close)
-#          # Hello button.
-#          self.hello_button = Button('Hello')
-#          self.placeControl(self.hello_button, 9, 1)
-#          # Connect Hello button.
-#          # self.connect(self.hello_button, lambda: xbmc.executebuiltin('Notification(Hello %s!, Welcome to PyXBMCt.)' % self.videos[1]['title']))
-#          # xbmc.executebuiltin('Notification(Hello %s!, Welcome to PyXBMCt.)' % self.name_field.getText()))
-#
-#     def set_navigation(self):
-#          """Set up keyboard/remote navigation between controls."""
-#          self.title_list.controlLeft(self.hello_button)
-#          # self.name_field.controlUp(self.hello_button)
-#          # self.name_field.controlDown(self.hello_button)
-#          self.close_button.controlLeft(self.hello_button)
-#          self.close_button.controlRight(self.hello_button)
-#          self.close_button.controlUp(self.title_list)
-#          self.close_button.controlDown(self.title_list)
-#          self.hello_button.controlLeft(self.close_button)
-#          self.hello_button.controlRight(self.close_button)
-#          self.hello_button.controlUp(self.title_list)
-#          self.hello_button.controlDown(self.title_list)
-#          # self.hello_button.setNavigation(self.name_field, self.name_field, self.close_button, self.close_button)
-#          # Set initial focus.
-#          self.setFocus(self.title_list)
 
 
 if __name__ == '__main__':
